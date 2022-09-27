e41ef7f83c696ab9222b218a860efd6aeebd62a5
christiangsn <christianguimaraes1996@gmail.com>
2022-09-27 20:17:03 -0300

    fix: move a files

--- src/domain/domain.module.ts
+++ src/domain/domain.module.ts
@@ -1,10 +1,14 @@
+import { GetAllStudentsResolver } from './graphql/resolvers/students/getAllStudents.resolver';
+import { EnrollmentsResolver } from './graphql/resolvers/enrollments.resolver';
+import { CoursesResolver } from './graphql/resolvers/courses.resolver';
 import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import path from 'path';
 import { InfraModule } from 'src/infra/infra.module';
-import { TestResolver } from './test.resolver';
+
+import { resolve } from 'path';
+import { GetAllStudentsRepository } from 'src/infra/gateways/prisma/repositories/students/getAllStudents.repository';
 
 @Module({
   imports: [
@@ -12,9 +16,14 @@ import { TestResolver } from './test.resolver';
     InfraModule,
     GraphQLModule.forRoot({
       driver: ApolloDriver,
-      autoSchemaFile: path.resolve(process.cwd(), 'src/schema.gql'),
+      autoSchemaFile: resolve(process.cwd(), 'src/schema.gql'),
     }),
   ],
-  providers: [TestResolver],
+  providers: [
+    CoursesResolver,
+    EnrollmentsResolver,
+    GetAllStudentsResolver,
+    { provide: 'IGetAllStudentsContract', useClass: GetAllStudentsRepository },
+  ],
 })
 export class DomainModule {}
